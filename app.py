3528162b8feb2a2c1ecc731ea99d13945d1bb7f8
samalbaddege <111477436+samalbaddege@users.noreply.github.com>
2022-11-22 18:32:14 +0530

    refuel record insert complete

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, request
-from dbhelper import selectAll
+from flask import Flask, render_template, request, redirect
+from dbhelper import selectAll, insertFuelEntry
 
 app = Flask(__name__)
 
@@ -8,12 +8,48 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route("/addRecord")
+@app.route("/addNew")
 def addRecord():
-    return render_template("addRecord.html")
+    return render_template("addNew.html")
 
+    # TODO
+    # Auto load last fuel price
+    # Show Vehicle name
+    # Show unit
+    # Show Fuel Type
+
+@app.route("/saveRecord", methods=["POST"])
+def saveRecord():
+    refuelMileage = request.form.get("refuelMileage")
+    refuelAmount = request.form.get("refuelAmount")
+    refuelType = request.form.get("refuelType")
+    refuelDate = request.form.get("refuelDate")
+    refuelQty = request.form.get("refuelQty")
+    refuelUnitPrice = request.form.get("refuelUnitPrice")
+    fuelType_idfuelType = request.form.get("fuelType_idfuelType")
+    Vehicle_idVehicle = request.form.get("Vehicle_idVehicle")
+    Units_idUnits = request.form.get("Units_idUnits")
+
+    refuelRecord = {
+        "refuelMileage":refuelMileage, 
+        "refuelAmount":refuelAmount,
+        "refuelType":refuelType,
+        "refuelDate":refuelDate,
+        "refuelQty":refuelQty,
+        "refuelUnitPrice":refuelUnitPrice,
+        "fuelType_idfuelType":fuelType_idfuelType,
+        "Vehicle_idVehicle":Vehicle_idVehicle,
+        "Units_idUnits":Units_idUnits
+    }
+
+    # print(refuelRecord)
+    # print(f"INSERT INTO refuelEntries (refuelDate, refuelMileage, refuelQty, refuelType, refuelUnitPrice, refuelAmount, fuelType_idfuelType, Vehicle_idVehicle, Units_idUnits) VALUES ('{refuelRecord['refuelDate']}','{refuelRecord['refuelMileage']}','{refuelRecord['refuelQty']}','{refuelRecord['refuelType']}','{refuelRecord['refuelUnitPrice']}','{refuelRecord['refuelAmount']}','{refuelRecord['fuelType_idfuelType']}','{refuelRecord['Vehicle_idVehicle']}','{refuelRecord['Units_idUnits']}')")
+    
+    insertFuelEntry(refuelRecord)
+
+    return redirect("/listRecords")
 
 @app.route("/listRecords")
 def listRecords():
-    RECORDS = selectAll("database.db", "refuelEntries")
+    RECORDS = selectAll("refuelEntries")
     return render_template("listRecords.html", records=RECORDS)
--- dbhelper.py
+++ dbhelper.py
@@ -18,8 +18,12 @@ def selectAll(tableName):
 
     return result
 
-#data is passed as a dictionary
-def insertFuelEntry():
+# data is passed as a dictionary
+# add type hinting
+def insertFuelEntry(rec: dict):
     conn = sqlite3.connect(DATABASE)
     cursor = conn.cursor()
-    cursor.execute(f"INSERT INTO refuelEntries (refuelDate, refuelMileage, refuelQty, refuelType, refuelUnitPrice, refuelAmount, fuelType_idfuelType, Vehicle_idVehicle, Units_idUnits) VALUES ()")
+    cursor.execute(f"INSERT INTO refuelEntries (refuelDate, refuelMileage, refuelQty, refuelType, refuelUnitPrice, refuelAmount, fuelType_idfuelType, Vehicle_idVehicle, Units_idUnits) VALUES ('{rec['refuelDate']}','{rec['refuelMileage']}','{rec['refuelQty']}','{rec['refuelType']}','{rec['refuelUnitPrice']}','{rec['refuelAmount']}','{rec['fuelType_idfuelType']}','{rec['Vehicle_idVehicle']}','{rec['Units_idUnits']}')")
+    conn.commit()
+    conn.close()
+
